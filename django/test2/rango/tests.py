dc518911d9ade7b66782aa4ee45902de2e111663
cxy627 <chenxuyisz@163.com>
2018-05-04 11:16:40 +0800

    180504

--- django/test2/rango/tests.py
+++ django/test2/rango/tests.py
@@ -8,6 +8,14 @@ from .models import Category, Page
 # Create your tests here.
 
 
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+
 class CategoryMethodTests(TestCase):
 
     def setUp(self):
@@ -58,3 +66,17 @@ class IndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "暂无类型")
         self.assertQuerysetEqual(response.context["categories"], [])
+
+    def test_index_view_with_categories(self):
+
+        add_cat("test", 1, 1)
+        add_cat("temp", 1, 1)
+        add_cat("tmp", 1, 1)
+        add_cat("tmp test temp", 1, 1)
+
+        response = self.client.get(reverse("rango:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+
+        num_cats = len(response.context["categories"])
+        self.assertEqual(num_cats, 4)
